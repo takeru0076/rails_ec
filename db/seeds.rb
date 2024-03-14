1f45914958f3bd5b62cd2138a53dc42cbbac6b44
takeru0076 <takeru200276@icloud.com>
2024-03-14 13:15:36 +0900

    Initial data was created using the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+p '==================== customer create ===================='
+Customer.create!(name: "Yamada Taro", email: "yamada.taro@gmail.com", password: "111111")
+Customer.create!(name: "Sato Mamoru", email: "sato.mamoru@gmail.com", password: "111111")
+Customer.create!(name: "Tanaka Saki", email: "tanaka.saki@gmail.com", password: "111111")
+Customer.create!(name: "Hoshino Yui", email: "hoshino.yui@gmail.com", password: "111111")
+Customer.create!(name: "Saito Kazuma", email: "saito.kazuma@gmail.com", password: "111111")
+
+p '==================== admin create ===================='
+Admin.create!(email: "admin@gmail.com", password: "1234qwer")
+
+p '==================== product create ===================='
+product1 = Product.new(
+  name: "Wool Comfort Hat",
+  description: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.", 
+  price: 4200, 
+  stock: 30
+)
+product1.image.attach(io: File.open(Rails.root.join('app/assets/images/hat.jpeg')), filename: 'hat.jpg')
+product1.save!
+
